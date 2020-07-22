084c89b12508d1a3fff0101c6f41d9dd12d9c0e5
LISH5 <727904507@qq.com>
2020-07-22 16:15:02 +0800

    [Shao Li][feature] pass Practice06Test

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,23 @@
 package practice06;
 
-public class Student {
+public class Student extends Person {
+    private Integer klass;
+
+    public Student(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + klass + ".";
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,32 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        klass = null;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        StringBuilder introResult = new StringBuilder();
+        introResult.append(super.introduce());
+        introResult.append(
+                (klass != null) ? " I am a Teacher. I teach Class " + klass + "." : " I am a Teacher. I teach No Class.");
+        return introResult.toString();
+    }
 }
